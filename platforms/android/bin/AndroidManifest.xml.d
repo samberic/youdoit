049ddd84ff405240829f4f4584ff1f8b64d6b546
samatkinson <sam@samatkinson.com>
2013-10-19 13:37:17 +0800

    set up for ios and android

---
+++ platforms/android/bin/AndroidManifest.xml.d
@@ -0,0 +1,2 @@
+/Users/samatkinson/Dev/hackathon/youdoit/platforms/android/bin/AndroidManifest.xml : \
+/Users/samatkinson/Dev/hackathon/youdoit/platforms/android/AndroidManifest.xml \
---
+++ platforms/android/gen/R.java.d
@@ -0,0 +1,9 @@
+/Users/samatkinson/Dev/hackathon/youdoit/platforms/android/gen/com/phonegap/helloworld/R.java \
+ : /Users/samatkinson/Dev/hackathon/youdoit/platforms/android/res/drawable/icon.png \
+/Users/samatkinson/Dev/hackathon/youdoit/platforms/android/res/drawable-hdpi/icon.png \
+/Users/samatkinson/Dev/hackathon/youdoit/platforms/android/res/drawable-ldpi/icon.png \
+/Users/samatkinson/Dev/hackathon/youdoit/platforms/android/res/drawable-mdpi/icon.png \
+/Users/samatkinson/Dev/hackathon/youdoit/platforms/android/res/drawable-xhdpi/icon.png \
+/Users/samatkinson/Dev/hackathon/youdoit/platforms/android/res/values/strings.xml \
+/Users/samatkinson/Dev/hackathon/youdoit/platforms/android/res/xml/config.xml \
+/Users/samatkinson/Dev/hackathon/youdoit/platforms/android/bin/AndroidManifest.xml \
